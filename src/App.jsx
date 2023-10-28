634e6afa901baf51d16f5688b857d69465199271
Rodrigo <rdg.hora1@gmail.com>
2023-10-28 08:38:22 -0300

    useCallback

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-import React, { useEffect, useState } from "react";
+import P from "prop-types";
+import React, { useCallback, useState } from "react";
 import "./App.css";
 
-const eventFn = () => {
-  console.log("H1 Clicado");
+const Button = React.memo(function Button({ incrementButton }) {
+  console.log("Filho");
+  return <button onClick={() => incrementButton(10)}>+</button>;
+});
+
+Button.propTypes = {
+  incrementButton: P.func,
 };
 
 export default function App() {
+  const [counter, setCounter] = useState(0);
+
   /* useState
   // A gente tem uma função passa o estado inicial para essa função e depois a gente pega do retorno dessa função
 
@@ -25,11 +33,11 @@ export default function App() {
   // };
  */
 
-  // useEffect
-  const [counter, setCounter] = useState(0);
+  /*  useEffect
+
   const [counter2, setCounter2] = useState(0);
 
-  // componentDidUpdate - executa toda vez que o component atualiza
+  componentDidUpdate - executa toda vez que o component atualiza
   useEffect(() => {
     console.log("componentDidUpdate");
   });
@@ -48,30 +56,21 @@ export default function App() {
   useEffect(() => {
     console.log("C1", counter, "C2", counter2);
   }, [counter, counter2]);
+ */
+
+  // useCallback vai renderizar a função sempre que a dependência mudar
+  const incrementCounter = useCallback((num) => {
+    // Usando o valor antigo do counter dessa maneira eu nao tenho mais dependências
+    setCounter((c) => c + num);
+  }, []);
+
+  console.log("PAI");
 
   return (
     <div className="App">
       <p>Teste2</p>
-      <h1>
-        C1: {counter} C2: {counter2}
-      </h1>
-      <button
-        type="button"
-        onClick={() => {
-          setCounter(counter + 1);
-        }}
-      >
-        C1 +
-      </button>
-
-      <button
-        type="button"
-        onClick={() => {
-          setCounter2(counter2 + 1);
-        }}
-      >
-        C2 +
-      </button>
+      <h1>C1: {counter}</h1>
+      <Button incrementButton={incrementCounter} />
     </div>
   );
 }
