6d7d5c91e4b72c99d9ffe4c2827a618a76b9d4ad
Rodrigo <rdg.hora1@gmail.com>
2023-10-29 08:18:32 -0300

    useContext

--- README.md
+++ README.md
@@ -46,7 +46,8 @@ desenvolvimento de interfaces interativas em JavaScript.
 o `useEffect` é uma função usada para realizar ações secundárias em resposta a mudanças em um
 componente ou quando algo acontece no ciclo de vida do componente. Aqui está como funciona:
 
-`Declaração do useEffect`: Você usa o `useEffect`em um componente funcional do React para realizar ações quando algo específico acontece. Para fazer isso, você passa duas coisas para o`useEffect`:
+`Declaração do useEffect`: Você usa o `useEffect`em um componente funcional do React para realizar
+ações quando algo específico acontece. Para fazer isso, você passa duas coisas para o`useEffect`:
 uma função e um array de dependências (ou nada, se você quiser que o efeito seja executado toda vez
 que o componente for renderizado).
 
@@ -279,3 +280,47 @@ Em resumo, o `useRef` é uma ferramenta útil para criar referências persistent
 variáveis em componentes do React. Isso é útil quando você precisa acessar ou modificar esses
 elementos ou variáveis, especialmente em casos em que o controle direto é necessário, como
 interações com o DOM ou animações. Use o `useRef` em vez de `document.querySelector` no React
+
+---
+
+## useContext() / createContext()
+
+`useContext` é um hook do React que permite o compartilhamento de dados e informações entre
+componentes, sem a necessidade de passar props manualmente de componente para componente. Ele é
+especialmente útil quando você deseja disponibilizar informações ou funcionalidades globais para
+vários componentes em sua aplicação, como temas, autenticação, preferências do usuário e muito mais.
+
+Aqui está uma explicação simplificada do `useContext`:
+
+1 - **Criando um contexto**: Você começa criando um contexto que atuará como um recipiente para os
+dados que deseja compartilhar.
+
+```js
+const MeuContexto = React.createContext();
+```
+
+2 - **Provedor de contexto**: Em um componente pai ou de nível superior da sua aplicação, você
+envolve os componentes que precisam acessar os dados desse contexto com um componente
+`<MeuContexto.Provider>`.
+
+```js
+<MeuContexto.Provider value={dadosCompartilhados}>
+  {/* Componentes que podem acessar dadosCompartilhados */}
+</MeuContexto.Provider>
+```
+
+3 - **Consumindo o contexto**: Nos componentes filhos, você pode acessar os dados do contexto usando
+o hook `useContext`. Isso permite que esses componentes acessem os dados compartilhados sem a
+necessidade de passar props.
+
+```js
+const dados = useContext(MeuContexto);
+```
+
+Isso simplifica o acesso a dados e funcionalidades globais em sua aplicação, tornando o código mais
+limpo e fácil de manter. `useContext` é especialmente útil para evitar a chamada de múltiplos níveis
+de componentes para passar dados de cima para baixo (props drilling).
+
+Por exemplo, você pode usar `useContext` para compartilhar informações como o tema atual da sua
+aplicação ou o estado de autenticação entre vários componentes sem precisar passar essas informações
+manualmente como props em todos os lugares onde elas são necessárias.
--- src/App.jsx
+++ src/App.jsx
@@ -1,119 +1,61 @@
-import P from "prop-types";
-import { useEffect, useMemo, useRef, useState } from "react";
-
+import { createContext, useContext, useState } from "react";
 import "./App.css";
 
-const Post = ({ post, handleClick }) => {
-  console.log("Filho renderizou");
+// eslint-disable-next-line
+const globalState = {
+  title: "O titulo do contexto",
+  body: "O body do contexto",
+  counter: 0,
+};
+// eslint-disable-next-line
+const GlobalContext = createContext();
+
+// eslint-disable-next-line
+const Div = ({ children }) => {
   return (
     <div className="App">
-      <h1 onClick={() => handleClick(post.title)}>{post.title}</h1>
-      <p>{post.body}</p>
+      <H1 />
+      <P />
     </div>
   );
 };
 
-Post.propTypes = {
-  post: P.shape({
-    title: P.string,
-    body: P.string,
-  }),
-  handleClick: P.func,
+// eslint-disable-next-line
+const H1 = () => {
+  const theContext = useContext(GlobalContext);
+  const {
+    contextState: { title, counter },
+  } = theContext;
+  return (
+    <h1>
+      {title} {counter}
+    </h1>
+  );
+};
+
+// eslint-disable-next-line
+const P = () => {
+  const theContext = useContext(GlobalContext);
+  const {
+    contextState: { body },
+    setContextState,
+  } = theContext;
+  return <p onClick={() => setContextState((s) => ({ ...s, counter: s.counter + 1 }))}>{body}</p>;
 };
 
 export default function App() {
-  /* useState
-  // A gente tem uma função passa o estado inicial para essa função e depois a gente pega do retorno dessa função
-
-  // Primeira posição valor do estado, Segunda posição função para setar o valor do estado
-  // const [reverse, setReverse] = useState(false);
-  // const [counter, setCounter] = useState(0);
-  // const reverseClass = reverse ? "reverse" : "";
-
-  // const handleClick = () => {
-  //   // setReverse((prevReverse) => !prevReverse);
-  //   setReverse(!reverse);
-  // };
-
-  // const handleIncrement = () => {
-  //   // setCounter((prevCounter) => prevCounter + 1);
-  //   setCounter(counter + 1);
-  // };
- */
-  /* useEffect
-
-  const [counter2, setCounter2] = useState(0);
-
-  componentDidUpdate - executa toda vez que o component atualiza
-  useEffect(() => {
-    console.log("componentDidUpdate");
-  });
-
-  // componentDidMount - executa 1x
-  useEffect(() => {
-    document.querySelector("h1")?.addEventListener("click", eventFn);
-
-    // componentWillUnmount - limpeza
-    return () => {
-      document.querySelector("h1")?.removeEventListener("click", eventFn);
-    };
-  }, []);
-
-  // Com dependência - executa toda vez que a dependência mudar
-  useEffect(() => {
-    console.log("C1", counter, "C2", counter2);
-  }, [counter, counter2]);
- */
-  /* useCallback
-    // useCallback vai renderizar a função sempre que a dependência mudar
-  const incrementCounter = useCallback((num) => {
-    // Usando o valor antigo do counter dessa maneira eu nao tenho mais dependências
-    setCounter((prevCounter) => prevCounter + num);
-  }, []);
-  */
-
-  // useMemo() Retorna um valor memoizado (guardado em cache)
-  // useCallback() Retorna um callback (Função) memoizado (guardado em cache).
-  // useRef serve para pegar alguma coisa que está na tela elemento da DOM, use no lugar do document.querySelector quando estiver usando React
-  const [posts, setPosts] = useState([]);
-  const [value, setValue] = useState("");
-  const input = useRef(null);
-  const contador = useRef(0);
-
-  console.log("Pai renderizou!");
-
-  // componentDidMount
-  useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/posts")
-      .then((response) => response.json())
-      .then((response) => setPosts(response));
-  }, []);
-
-  useEffect(() => {
-    input.current.focus();
-    console.log(input.current);
-  }, [value]);
-
-  // componentDidUpdate
-  useEffect(() => {
-    contador.current++;
-  });
-
-  const handleClick = (value) => {
-    setValue(value);
-  };
+  const [contextState, setContextState] = useState(globalState);
 
   return (
-    <div className="App">
-      <h1>Renderizou {contador.current}x</h1>
-      <input ref={input} type="search" value={value} onChange={(e) => setValue(e.target.value)} />
-      {useMemo(() => {
-        return posts.length > 0 ? (
-          posts.map((post) => <Post post={post} key={post.id} handleClick={handleClick} />)
-        ) : (
-          <p>Ainda Não existem posts</p>
-        );
-      }, [posts])}
-    </div>
+    <GlobalContext.Provider value={{ contextState, setContextState }}>
+      <Div />
+    </GlobalContext.Provider>
   );
 }
+
+// useState() - é um gancho React que permite adicionar uma variável de estado ao componente.
+// useEffect() - é um gancho React que permite sincronizar um componente com um sistema externo.
+// useCallback() - Retorna um callback (Função) memoizado (guardado em cache).
+// useMemo() - Retorna um valor memoizado (guardado em cache).
+// useRef() - serve para pegar alguma coisa que está na tela elemento da DOM, use no lugar do document.querySelector quando estiver usando React.
+// useContext()/React.createContext() - permite eu transforma algo em global para ser usado em outro lugar sem precisar ficando passando pelas props
