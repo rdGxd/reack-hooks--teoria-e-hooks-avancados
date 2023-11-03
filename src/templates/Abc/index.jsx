df993cc9d37bf6471f8c220fd529af093ecb62db
Rodrigo Silva <seu_email@email.com>
2023-11-03 10:44:57 -0300

    useNavigate para redirecionar o usuario

--- src/templates/Abc/index.jsx
+++ src/templates/Abc/index.jsx
@@ -1,7 +1,16 @@
-import { useParams } from "react-router-dom";
+import { useEffect } from "react";
+import { useLocation, useNavigate, useParams } from "react-router-dom";
 
 export const Abc = () => {
   const { slug, id } = useParams();
+  const location = useLocation();
+  const history = useNavigate();
+
+  useEffect(() => {
+    setTimeout(() => {
+      history("/");
+    }, 3000);
+  }, [history]);
 
   return (
     <h1>
